6353ebfe7c6a5cd8e27556a59eb96435891a79e6
ɴᴀᴋ ᵏᵉ <93425656+alphanakflix@users.noreply.github.com>
2023-02-13 16:04:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -28,8 +28,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1458235021"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://nakflixtv:alpha3720@cluster00.09dhvl5.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluster00")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001892458448"))
